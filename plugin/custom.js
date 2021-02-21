3fb8f224c54276cddfbca9bd4192e255d2779213
林伶 <linling@linlingdeMacBook-Pro.local>
2021-02-21 23:16:01 +0800

    履歷上傳

---
+++ plugin/custom.js
@@ -0,0 +1,42 @@
+$(document).ready(function(){
+
+  $("#myMenu a").click(function () {
+    const theid = $(this).attr("href");
+    const val = $(theid).offset().top - $("#myMenu").innerHeight() + 1;
+    $("html").animate(
+      { scrollTop: val }, 1000, "swing"
+    );
+  });
+
+  function spy() { //檢查目前滾動位置與對應MENU之ACTIVE
+    const nowat = $(document).scrollTop();
+    $("section").each(function () {
+      const
+        id = $(this).attr('id'), // id = this.id,
+        offset = $(this).offset().top - $("#myMenu").innerHeight(),
+        height = $(this).innerHeight();
+
+      if (offset <= nowat && nowat < offset + height) { //目前高度落於該section內
+        $('#myMenu a').not(`a[href='#${id}']`).removeClass('active');
+        $(`#myMenu a[href='#${id}']`).addClass('active');
+      }
+    });
+  }
+  spy();
+
+  $(document).scroll(function(){
+    spy();
+  });
+
+  $(window).resize();
+  spy();
+
+
+
+
+
+
+
+
+
+})
